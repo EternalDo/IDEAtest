9582702d18f611f2e9eba7367b30904ae1d4295e
duyongheng <1325558450@qq.com>
2018-08-30 00:40:23 +0800

    第一次测试

---
+++ src/main/java/com/yongheng/conloller/Test.java
@@ -0,0 +1,16 @@
+package com.yongheng.conloller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @RequestMapping("/hello")
+    public  String getHello(){
+
+        return "helloooooooooo";
+
+    }
+
+}
